58011c90d9428b85b65edc5f67fa4a34050b5dfb
benittoraj <brendonbeni42@gmail.com>
2020-08-27 23:52:06 +0530

    Updates

---
+++ precache-manifest.3b40ee70b7daef112d429daf2f8fba25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b333177041fda8da1880a768b73b61",
+    "url": "/covid19-india-status/index.html"
+  },
+  {
+    "revision": "0078b26d3f58f0e8cda2",
+    "url": "/covid19-india-status/static/css/main.a845c38f.chunk.css"
+  },
+  {
+    "revision": "b0d196ef94ba3d9679a9",
+    "url": "/covid19-india-status/static/js/2.da6b3cb1.chunk.js"
+  },
+  {
+    "revision": "e44abebdb2f4ceb2123260762b9febf0",
+    "url": "/covid19-india-status/static/js/2.da6b3cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0078b26d3f58f0e8cda2",
+    "url": "/covid19-india-status/static/js/main.cd3d4ab3.chunk.js"
+  },
+  {
+    "revision": "bc6f741cb97b48c6eea5",
+    "url": "/covid19-india-status/static/js/runtime-main.df98c654.js"
+  }
+]);
